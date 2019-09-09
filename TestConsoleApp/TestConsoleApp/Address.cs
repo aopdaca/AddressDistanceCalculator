a4be741e550f22a6dffaa5b9e3fd678bef38eadc
Antonio Garcia <agarcia347@student.rccd.edu>
2019-09-09 16:28:42 -0500

    added the test console app to test distances between two addresses

---
+++ TestConsoleApp/TestConsoleApp/Address.cs
@@ -0,0 +1,106 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Text.RegularExpressions;
+
+namespace TestConsoleApp
+{
+    public class Address
+    {
+        public string _zip;
+        public string _state;
+        public string _city;
+        public string _streetaddress;
+        public int _addressId;
+
+
+        public int AddressId
+        {
+            get => _addressId;
+            set
+            {
+                if (value > 0)
+                {
+                    _addressId = value;
+                }
+                else
+                {
+                    throw new ArgumentException(String.Format("{0} is less than 0", value), "value");
+                }
+            }
+        }
+
+        public string StreetAddress
+        {
+            get => _streetaddress;
+            set
+            {
+                if (value != null)
+                {
+                    _streetaddress = value;
+                }
+                else
+                {
+                    throw new ArgumentNullException(String.Format("{0} is Null", value), "value");
+                }
+
+            }
+        }
+        public string City
+        {
+            get => _city;
+            set
+            {
+                if (Regex.IsMatch(value, @"^[a-zA-Z]+$") && value != null)
+                {
+                    _city = value;
+                }
+                else if (value == null)
+                {
+                    throw new ArgumentNullException(String.Format("{0} is Null", value), "value");
+                }
+                else
+                {
+                    throw new ArgumentException(String.Format("{0} is not a string of only letters", value), "value");
+                }
+
+            }
+        }
+        public string State
+        {
+            get => _state;
+            set
+            {
+                if (Regex.IsMatch(value, @"^[a-zA-Z]+$") && value != null)
+                {
+                    _state = value;
+                }
+                else if (value == null)
+                {
+                    throw new ArgumentNullException(String.Format("{0} is Null", value), "value");
+                }
+                else
+                {
+                    throw new ArgumentException(String.Format("{0} is not a string of only letters", value), "value");
+                }
+
+            }
+        }
+        public string Zipcode
+        {
+            get => _zip;
+            set
+            {
+                if (Regex.IsMatch(value, @"^[0-9]+$") && value.Length == 5)
+                {
+                    _zip = value;
+                }
+                else
+                {
+                    throw new ArgumentException(String.Format("{0} is not a string of 5 integers", value), "value");
+                }
+
+            }
+        }
+    }
+}
---
+++ TestConsoleApp/TestConsoleApp/Program.cs
@@ -0,0 +1,107 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Threading.Tasks;
+
+namespace TestConsoleApp
+{
+    class Program
+    {
+        private static string _apiKey = "AIzaSyCxYMcmEjlHQ2r2CywMgyK7YEplxurqW2A";
+
+        static async Task Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Address one = new Address();
+            one.StreetAddress = "912 Seymour Way";
+            one.City = "Perris";
+            one.State = "Ca";
+            one.Zipcode = "92571";
+            Address two = new Address();
+            two.StreetAddress = "3579 Comer Ave";
+            two.City = "Riverside";
+            two.State = "Ca";
+            two.Zipcode = "92507";
+            Console.WriteLine(GetDistanceAsync(one, two).GetAwaiter().GetResult());
+        }
+        public static async System.Threading.Tasks.Task<string> GetDistanceAsync(Address origin, Address destination)
+        {
+
+            string formatedOrigin = formatAddress(origin);
+            Console.WriteLine(formatedOrigin);
+            string formatedDestination = formatAddress(destination);
+            Console.WriteLine(formatedDestination);
+            string googleApiUrlParameter = GetGoogleApiUrl(formatedOrigin, formatedDestination);
+            string googleBaseUrl = "https://maps.googleapis.com/maps/api/distancematrix/json";
+
+            DistanceMatrixResponse deserialized;
+
+            HttpClient client = new HttpClient();
+            client.BaseAddress = new Uri(googleBaseUrl);
+
+            // Add an Accept header for JSON format.
+            client.DefaultRequestHeaders.Accept.Add(
+            new MediaTypeWithQualityHeaderValue("application/json"));
+
+            // List data response.
+            HttpResponseMessage response = client.GetAsync(googleApiUrlParameter).Result;  // Blocking call! Program will wait here until a response is received or a timeout occurs.
+            if (response.IsSuccessStatusCode)
+            {
+                Console.WriteLine(await response.Content.ReadAsStringAsync());
+
+                deserialized = JsonConvert.DeserializeObject<DistanceMatrixResponse>(await response.Content.ReadAsStringAsync());
+                // Parse the response body.
+                Console.WriteLine(deserialized.rows[0].elements[0].distance.text);
+                return deserialized.rows[0].elements[0].distance.text;
+            }
+            else
+            {
+                Console.WriteLine("{0} ({1})", (int)response.StatusCode, response.ReasonPhrase);
+                return "";
+            }
+
+            //Make any other calls using HttpClient here.
+
+            //Dispose once all HttpClient calls are complete. This is not necessary if the containing object will be disposed of; for example in this case the HttpClient instance will be disposed automatically when the application terminates so the following call is superfluous.
+            //client.Dispose();
+
+        }
+        public static string GetGoogleApiUrl(string origin, string destination)
+        {
+            return $"?units=imperial&origins={origin}&destinations={destination}&key=" + _apiKey;
+        }
+        public static string formatAddress(Address address)
+        {
+            return address._streetaddress.Replace(" ", "+") + "+" + address._city.Replace(" ", "+") + "," + address._state.Replace(" ", "+") + "+" + address._zip; ;
+        }
+    }
+    public class DistanceMatrixResponse
+    {
+        public string[] destination_addresses { get; set; }
+        public string[] origin_addresses { get; set; }
+        public Row[] rows { get; set; }
+        public string status { get; set; }
+
+    }
+    public class Row
+    {
+        public Element[] elements { get; set; }
+    }
+    public class Element
+    {
+        public Distance distance { get; set; }
+        public Duration duration { get; set; }
+        public string status { get; set; }
+    }
+    public class Distance
+    {
+        public string text { get; set; }
+        public string value { get; set; }
+    }
+    public class Duration
+    {
+        public string text { get; set; }
+        public string value { get; set; }
+    }
+}
